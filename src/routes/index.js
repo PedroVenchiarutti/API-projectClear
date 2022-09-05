a7097a6ff8c5afbe3bff50986c376034bc68c9fc
Smooke09 <pedrolucas1014zz@gmail.com>
2022-09-05 10:23:53 -0300

    request

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,21 @@
-const express = require('express');
+const express = require("express");
 
-// middleware 
-const authMiddleware = require('../middlewares/auth.js')
+// middleware
+const authMiddleware = require("../middlewares/auth.js");
 
-const privateRoutes = require('./privateRoutes.js');
-const publicRoutes = require('./publicRoutes.js');
+const privateRoutes = require("./privateRoutes.js");
+const publicRoutes = require("./publicRoutes.js");
 const privateClientRoutes = require("./privateClientRoutes.js");
 
 const Routes = express.Router();
 
+// middleware validando as rotas
+/* 
 Routes.use("/protected", authMiddleware(false), privateClientRoutes);
-Routes.use("/protected", authMiddleware(true), privateRoutes);
+Routes.use("/protected", authMiddleware(false), privateRoutes); */
+
+Routes.use("/protected", privateClientRoutes);
+Routes.use("/protected", privateRoutes);
 Routes.use("/public", publicRoutes);
 
 module.exports = Routes;
