3414b2cb8f7b34fc45eb283ba7131e2314c2bb41
Ryan Gualberto <ryanoliveiragualberto@gmail.com>
2022-09-09 12:14:50 -0300

    change in method search product

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -1,14 +1,16 @@
-const apiError = require('../error/apiError.js');
-const productRepository = require('../repositories/productRepository.js');
+const apiError = require("../error/apiError.js");
+const productRepository = require("../repositories/productRepository.js");
 
 exports.getAll = async (req, res, next) => {
-  productRepository.list(req.params.num)
-    .then(list => {
+  productRepository.list(req.params.num).then(
+    (list) => {
       res.send(list);
-    }, (e) => {
-      next(apiError.badRequest(e.message))
-    })
-}
+    },
+    (e) => {
+      next(apiError.badRequest(e.message));
+    }
+  );
+};
 
 exports.getById = (req, res, next) => {
   /**
@@ -18,16 +20,18 @@ exports.getById = (req, res, next) => {
 
   const id = req.params.id;
 
-  productRepository.select("products", id)
-    .then(product => {
+  productRepository.select("products", id).then(
+    (product) => {
       res.send(product);
-    }, (e) => {
-      next(apiError.badRequest(e.message))
-    })
-}
+    },
+    (e) => {
+      next(apiError.badRequest(e.message));
+    }
+  );
+};
 
 exports.search = (req, res, next) => {
-/**
+  /**
     #swagger.tags = ['product']
     #swagger.summary="Busca um produto."
     #swagger.parameters['product'] => {
@@ -38,15 +42,17 @@ exports.search = (req, res, next) => {
       }
     }
     */
-  const param = req.body.search;
-  
-  productRepository.search(param)
-    .then(results => {
+  const param = req.params.search;
+
+  productRepository.search(param).then(
+    (results) => {
       res.send(results);
-    }, (e) => {
-      next(apiError.badRequest(e.message))
-    })
-}
+    },
+    (e) => {
+      next(apiError.badRequest(e.message));
+    }
+  );
+};
 
 exports.add = (req, res, next) => {
   /**
@@ -68,16 +74,17 @@ exports.add = (req, res, next) => {
   const product = req.body;
   console.log(product);
 
-  productRepository.insertTable("products", product)
-    .then(response => {
+  productRepository.insertTable("products", product).then(
+    (response) => {
       res.send();
-    }, (e) => {
-      next(apiError.badRequest(e.message))
-    })
-}
+    },
+    (e) => {
+      next(apiError.badRequest(e.message));
+    }
+  );
+};
 
 exports.update = (req, res, next) => {
-
   /**
     #swagger.tags = ['product']
     #swagger.summary="Atualizacao dos dados de um produto"
@@ -96,13 +103,15 @@ exports.update = (req, res, next) => {
 
   const product = req.body;
 
-  productRepository.updateTable("products", product)
-    .then(response => {
-      res.send()
-    }, (e) => {
-      next(apiError.badRequest(e.message))
-    })
-}
+  productRepository.updateTable("products", product).then(
+    (response) => {
+      res.send();
+    },
+    (e) => {
+      next(apiError.badRequest(e.message));
+    }
+  );
+};
 
 exports.remove = (req, res, next) => {
   /**
@@ -116,11 +125,13 @@ exports.remove = (req, res, next) => {
    */
 
   const id = req.params.id;
-  console.log(id)
-  productRepository.deleteTable('products', id)
-    .then(response => {
+  console.log(id);
+  productRepository.deleteTable("products", id).then(
+    (response) => {
       res.send();
-    }, (e) => {
-      next(apiError.badRequest(e.message))
-    })
-}
+    },
+    (e) => {
+      next(apiError.badRequest(e.message));
+    }
+  );
+};
