b650c640c19d94b2ea9dd318bb48bc43aaf420e4
Douglas <douglasvitor.dev@gmail.com>
2022-09-12 11:44:46 -0300

    Favorites treatment

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -3,8 +3,7 @@ const apiError = require('../error/apiError.js');
 require('dotenv/config');
 
 const authMiddleware = (permision = false ) => (req, res, next) => {
-  const token = req.headers['authorization'];
-  console.log(token);                                                                                                                                                   
+  const token = req.headers['authorization'];                                                                                                                                        
   if (token) jwt.verify(token, process.env.SECRET, (err, decoded) => {
     if (err) next(apiError.forbidden("acesso negado"))
     else next();
