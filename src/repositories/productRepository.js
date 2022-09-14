9363e44f78b308a21a0698a90205b45687c6fda9
Douglas <douglasvitor.dev@gmail.com>
2022-09-14 10:56:01 -0300

    Creating route to get all products brands

--- src/repositories/productRepository.js
+++ src/repositories/productRepository.js
@@ -26,6 +26,14 @@ class ProductRepository extends genericQuerys {
         })
     });
   }
+
+  static getBrands() {
+    return new Promise((resolve, reject) => {
+      db.exec(`SELECT DISTINCT brand FROM products`)
+        .then(results => resolve(results))
+        .catch(error => reject(error));
+    });
+  }
 }
 
 module.exports = ProductRepository;
